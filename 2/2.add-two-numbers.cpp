18b4c055b7a0c08cd7c8340d96cef8b9802e5a6f
Rich Nakasato <richnakasato@hotmail.com>
2018-01-03 15:35:58 -0500

    first passing submission for #2, but slower than ~53% of submissions

--- 2/2.add-two-numbers.cpp
+++ 2/2.add-two-numbers.cpp
@@ -36,6 +36,50 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+      ListNode* head = nullptr;
+      ListNode* curr = nullptr;
+      ListNode* t1 = l1;
+      ListNode* t2 = l2;
+      int v1, v2, v3;
+      bool c = false;
+      while(!(t1==nullptr && t2==nullptr)) {
+        v1 = extractVal(t1);
+        v2 = extractVal(t2);
+        v3 = v1 + v2;
+        if(c) {
+          v3++;
+          c = false;
+        }
+        if(v3 > 9) {
+          v3 = v3 % 10;
+          c = true;
+        }
+        ListNode* lnode = new ListNode(v3);
+        if(head == nullptr) {
+          head = lnode;
+          curr = head;
+        }
+        else {
+          curr->next = lnode; // append new node onto our list
+          curr = lnode; // set current pointer to new node
+        }
+        if(t1!=nullptr) {
+          t1 = t1->next;
+        }
+        if(t2!=nullptr) {
+          t2 = t2->next;
+        }
+      }
+      // remember to append 1 if we have a remaining carry bit
+      if(c) {
+        ListNode* lnode = new ListNode(1);
+        curr->next = lnode;
+      }
+      return head;
+    }
 
+private:
+    int extractVal(ListNode* node) {
+      return (node == nullptr) ? 0 : node->val;
     }
 };
