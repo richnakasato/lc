f89e5be27ea96d7d888a4103807ae3f788baeca1
Rich Nakasato <richnakasato@hotmail.com>
2018-01-05 11:14:58 -0500

    working (but slow) problem 3

--- 3/3.longest-substring-without-repeating-characters.cpp
+++ 3/3.longest-substring-without-repeating-characters.cpp
@@ -24,6 +24,32 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
+      int str_len = s.length();
+      int max_len = 0;
+      int cur_len = 0;
+      int start_idx = 0;
+      int end_idx = 0;
+      set<char> seen_chars = {};
 
+      if(s=="") { return 0; }
+
+      char start_char = s[start_idx];
+      char end_char = s[end_idx];
+      while(end_idx < str_len) {
+        auto search = seen_chars.find(end_char);
+        while(search != seen_chars.end()) {
+          seen_chars.erase(start_char);
+          cur_len--;
+          start_idx++;
+          start_char = s[start_idx];
+          search = seen_chars.find(end_char);
+        }
+        seen_chars.insert(end_char);
+        cur_len++;
+        end_idx++;
+        end_char = s[end_idx];
+        if(cur_len > max_len) { max_len = cur_len; }
+      }
+      return max_len;
     }
 };
