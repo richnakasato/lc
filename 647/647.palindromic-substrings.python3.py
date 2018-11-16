f9196bcb838398ab5b46e96fcfa685029d834d83
Rich Nakasato <richnakasato@hotmail.com>
2018-11-15 20:50:14 -0500

    implemented BF BF, looked up smarter BF, need to understand manacher's

--- 647/647.palindromic-substrings.python3.py
+++ 647/647.palindromic-substrings.python3.py
@@ -9,37 +9,37 @@
 # Total Submissions: 120.9K
 # Testcase Example:  '"abc"'
 #
-# 
+#
 # Given a string, your task is to count how many palindromic substrings in this
 # string.
-# 
-# 
-# 
+#
+#
+#
 # The substrings with different start indexes or end indexes are counted as
-# different substrings even they consist of same characters. 
-# 
-# 
+# different substrings even they consist of same characters.
+#
+#
 # Example 1:
-# 
+#
 # Input: "abc"
 # Output: 3
 # Explanation: Three palindromic strings: "a", "b", "c".
-# 
-# 
-# 
+#
+#
+#
 # Example 2:
-# 
+#
 # Input: "aaa"
 # Output: 6
 # Explanation: Six palindromic strings: "a", "a", "a", "aa", "aa", "aaa".
-# 
-# 
-# 
+#
+#
+#
 # Note:
-# 
+#
 # The input string length won't exceed 1000.
-# 
-# 
+#
+#
 #
 class Solution:
     def countSubstrings(self, s):
@@ -47,4 +47,24 @@ class Solution:
         :type s: str
         :rtype: int
         """
-        
+        # naive brute force version (i.e. my version)
+        count = 0
+        for start in range(len(s)):
+            for end in range(start+1, len(s+1)):
+                curr = s[start:end]
+                if curr == curr[::-1]:
+                    count += 1
+        return count
+
+    def countSubstrings2(self, s):
+        # smart brute force - LC solution version (had to look this one up)
+        count = 0
+        n = len(s)
+        for center in range(2*n-1):
+            left = center//2
+            right = left + center%2
+            if left >= 0 and right < n and s[left] == s[right]:
+                count += 1
+                left -= 1
+                right += 1
+        return count
