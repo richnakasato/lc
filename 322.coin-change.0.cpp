5a521cfd03b2d61097a2545605f7180079c0cb02
Rich Nakasato <richnakasato@hotmail.com>
2019-02-15 17:20:55 -0500

    implemented

--- 322.coin-change.0.cpp
+++ 322.coin-change.0.cpp
@@ -32,19 +32,39 @@
  * You may assume that you have an infinite number of each kind of coin.
  *
  */
-bool compare(int& left, int& right) {
-    return left > right;
-}
-
 class Solution {
 public:
-    int helper()
-    {
-        int combo = 0;
-        return 0;
-    }
-    int coinChange(vector<int>& coins, int amount)
-    {
-        return 0;
+    int coinChange(vector<int>& coins, int amount) {
+        if (amount == 0) return 0;
+        if (coins.empty()) return -1;
+        std::sort(coins.begin(), coins.end());
+        if (amount < coins[0]) return -1;
+
+        std::vector<std::vector<int>> memo(coins.size()+1,
+                                           std::vector<int>(amount+1,-1));
+        memo[0][0] = 0;
+        for (size_t curr_coin=0; curr_coin<coins.size(); ++curr_coin) {
+            for (int curr_amount=1; curr_amount<=amount; ++curr_amount) {
+                int prev_amount = curr_amount - coins[curr_coin];
+                if (prev_amount >= 0) {
+                    int min_count = std::numeric_limits<int>::max();
+                    for (size_t prev_coin=0; prev_coin<=curr_coin+1; ++prev_coin) {
+                        int prev_count = memo[prev_coin][prev_amount];
+                        if (prev_count != -1) {
+                            min_count = std::min(min_count, prev_count);
+                        }
+                    }
+                    int val = min_count != std::numeric_limits<int>::max() ?
+                              1 + min_count : -1;
+                    memo[curr_coin+1][curr_amount] = val;
+                }
+            }
+        }
+        int min_ways = std::numeric_limits<int>::max();
+        for (size_t i=0; i<memo.size(); ++i) {
+            if (memo[i][amount] != -1)
+                min_ways = std::min(min_ways, memo[i][amount]);
+        }
+        return min_ways != std::numeric_limits<int>::max() ? min_ways : -1;
     }
 };
