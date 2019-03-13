46de49d64436ad7b726c9f55d771cfa754a0a441
Rich Nakasato <richnakasato@hotmail.com>
2019-03-13 15:12:08 -0400

    still working on implementation

---
+++ 215.kth-largest-element-in-an-array.1.cpp
@@ -0,0 +1,63 @@
+/*
+ * [215] Kth Largest Element in an Array
+ *
+ * https://leetcode.com/problems/kth-largest-element-in-an-array/description/
+ *
+ * algorithms
+ * Medium (44.28%)
+ * Total Accepted:    288.1K
+ * Total Submissions: 650.7K
+ * Testcase Example:  '[3,2,1,5,6,4]\n2'
+ *
+ * Find the kth largest element in an unsorted array. Note that it is the kth
+ * largest element in the sorted order, not the kth distinct element.
+ *
+ * Example 1:
+ *
+ *
+ * Input: [3,2,1,5,6,4] and k = 2
+ * Output: 5
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: [3,2,3,1,2,4,5,5,6] and k = 4
+ * Output: 4
+ *
+ * Note:
+ * You may assume k is always valid, 1 ≤ k ≤ array's length.
+ *
+ */
+class Solution {
+    size_t pivot(std::vector<int>& nums, size_t start, size_t end) {
+        int pivot = nums[end];
+        size_t p_idx = start;
+        for (size_t i=start; i<end; ++i) {
+            if (nums[i] < nums[pivot]) {
+                std::swap(nums[i], nums[p_idx++]);
+            }
+        }
+        std::swap(nums[end], nums[p_idx]);
+        return p_idx;
+    }
+
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        int start = 0;
+        int end = nums.size()-1;
+        int kth = 0;
+        while (start < end) {
+            int p_idx = pivot(nums, start, end);
+            // need to figure this part out...
+            kth = nums.size() - p_idx + 1;
+            if (k < p_idx) {
+                start = p_idx+1;
+            }
+            else {
+                end = p_idx-1;
+            }
+        }
+        return nums[kth];
+    }
+};
