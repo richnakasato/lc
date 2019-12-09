f36ca326e71e67e228700cc887341d4948a980f2
Rich Nakasato <richnakasato@hotmail.com>
2019-12-09 07:16:22 -0500

    implemented

---
+++ 230.kth-smallest-element-in-a-bst.1.cpp
@@ -0,0 +1,84 @@
+/*
+ * [230] Kth Smallest Element in a BST
+ *
+ * https://leetcode.com/problems/kth-smallest-element-in-a-bst/description/
+ *
+ * algorithms
+ * Medium (48.56%)
+ * Total Accepted:    183.4K
+ * Total Submissions: 377.7K
+ * Testcase Example:  '[3,1,4,null,2]\n1'
+ *
+ * Given a binary search tree, write a function kthSmallest to find the kth
+ * smallest element in it.
+ *
+ * Note:
+ * You may assume k is always valid, 1 ≤ k ≤ BST's total elements.
+ *
+ * Example 1:
+ *
+ *
+ * Input: root = [3,1,4,null,2], k = 1
+ * ⁠  3
+ * ⁠ / \
+ * ⁠1   4
+ * ⁠ \
+ * 2
+ * Output: 1
+ *
+ * Example 2:
+ *
+ *
+ * Input: root = [5,3,6,2,4,null,null,1], k = 3
+ * ⁠      5
+ * ⁠     / \
+ * ⁠    3   6
+ * ⁠   / \
+ * ⁠  2   4
+ * ⁠ /
+ * ⁠1
+ * Output: 3
+ *
+ *
+ * Follow up:
+ * What if the BST is modified (insert/delete operations) often and you need to
+ * find the kth smallest frequently? How would you optimize the kthSmallest
+ * routine?
+ *
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int kthSmallest(TreeNode* root, int k) {
+        int count = 1;
+        if (!root) return 0;
+        bool done = false;
+        std::vector<TreeNode*> in_order;
+        TreeNode* curr = root;
+        while (!done) {
+            if (curr) {
+                in_order.push_back(curr);
+                curr = curr->left;
+            } else {
+                if (!in_order.empty()) {
+                    curr = in_order.back();
+                    in_order.pop_back();
+                    if (count == k) return curr->val;
+                    count++;
+                    curr = curr->right;
+                } else {
+                    done = true;
+                }
+            }
+        }
+        return -1;
+    }
+};
