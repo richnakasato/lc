b3cf9943597edd74d15af75c97b63afac7703b65
Rich Nakasato <richnakasato@hotmail.com>
2020-05-15 22:07:26 -0400

    implemented

---
+++ 30days_042020/26042020_longest_common_subsequence.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        if (text1.empty() || text2.empty()) return 0;
+        auto memo = vector<vector<int>>(text1.size() + 1, vector<int>(text2.size() + 1, 0));
+        for (auto r=1; r<=text1.size(); ++r) {
+            for (auto c=1; c<=text2.size(); ++c) {
+                if (text1[r-1] == text2[c-1]) {
+                    memo[r][c] = memo[r-1][c-1] + 1;
+                } else {
+                    memo[r][c] = max(memo[r-1][c], memo[r][c-1]);
+                }
+            }
+        }
+        return memo[text1.size()][text2.size()];
+    }
+};
