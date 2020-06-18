b8e476c0275f6d8d3fffc505f00659dcfbdfa032
Rich Nakasato <richnakasato@hotmail.com>
2020-06-17 22:05:54 -0400

    added dfs version

--- challenge_062020/17062020_surrounded_regions.cpp
+++ challenge_062020/17062020_surrounded_regions.cpp
@@ -9,33 +9,28 @@ public:
         if (c < 0 or board[r].size() <= c) return false;
         return board[r][c] == 'O';
     }
+    void helper(vector<vector<char>>& board, int r, int c) {
+        board[r][c] = 'X';
+        auto moves = vector<vector<int>>{{r-1,c},{r+1,c},{r,c-1},{r,c+1}};
+        for (auto move : moves) {
+            auto next_r = move[0];
+            auto next_c = move[1];
+            if (canMove(board, next_r, next_c)) helper(board, next_r, next_c);
+        }
+        return;
+    }
     void solve(vector<vector<char>>& board) {
         if (board.size() < 3 || board[0].size() < 3) return;
         int max_r = board.size();
         int max_c = board[0].size();
-        auto q = queue<vector<int>>{};
+        auto flip = board;
         for (auto r=0; r<max_r; ++r) {
-            if (canMove(board, r, 0)) q.push({r,0});
-            if (canMove(board, r, max_c-1)) q.push({r,max_c-1});
+            if (canMove(flip, r, 0)) helper(flip, r, 0);
+            if (canMove(flip, r, max_c-1)) helper(flip, r, max_c-1);
         }
         for (auto c=0; c<max_c; ++c) {
-            if (canMove(board, 0, c)) q.push({0,c});
-            if (canMove(board, max_r-1, c)) q.push({max_r-1,c});
-        }
-        auto flip = board;
-        while (!q.empty()) {
-            auto temp = q.front(); q.pop();
-            auto r = temp[0];
-            auto c = temp[1];
-            flip[r][c] = 'X';
-            auto moves = vector<vector<int>>{{r-1,c},{r+1,c},{r,c-1},{r,c+1}};
-            for (auto move : moves) {
-                auto next_r = move[0];
-                auto next_c = move[1];
-                if (canMove(flip, next_r, next_c)) {
-                    q.push({next_r,next_c});
-                }
-            }
+            if (canMove(flip, 0, c)) helper(flip, 0, c);
+            if (canMove(flip, max_r-1, c)) helper(flip, max_r-1, c);
         }
         for (auto r=0; r<max_r; ++r) {
             for (auto c=0; c<max_c; ++c) {
